1baab054322bdb7d996b1f0d86afd6d6b09b9b7a
jonasdemoor <17252323+jonasdemoor@users.noreply.github.com>
2021-03-15 14:03:29 +0100

    Parameterize system accounting class

--- manifests/system.pp
+++ manifests/system.pp
@@ -1,13 +1,19 @@
-# @api private
-#
 # This class provides a solution to enable accounting
 #
-class systemd::system {
-  $systemd::accounting.each |$option, $value| {
+# @param config_path
+#   Filesystem path to the system configuration ile
+# @param accounting
+#   Systemd accounting rules to set up
+#
+class systemd::system(
+  Stlib::Unixpath $config_path,
+  Hash[String, String] $accounting = {},
+) {
+  $accounting.each |$option, $value| {
     ini_setting { $option:
       ensure  => 'present',
-      path    => '/etc/systemd/system.conf',
       section => 'Manager',
+      path    => $config_path,
       setting => $option,
       value   => $value,
     }
