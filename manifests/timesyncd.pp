1712c2a39957e9671f9ed52e459459a1910b41b7
jonasdemoor <17252323+jonasdemoor@users.noreply.github.com>
2021-03-15 14:44:38 +0100

    Parameterize timesyncd class

--- manifests/timesyncd.pp
+++ manifests/timesyncd.pp
@@ -1,7 +1,10 @@
 # This class provides an abstract way to trigger systemd-timesyncd
 #
-# @param ensure
-#   The state that the ``networkd`` service should be in
+# @param service_name
+#   Name of the timesyncd service to manage
+#
+# @param config_path
+#   Filesystem path to the timesyncd configuration file
 #
 # @param ntp_server
 #   A space-separated list of NTP servers, will be combined with interface specific
@@ -11,21 +14,51 @@
 #   A space-separated list of NTP server host names or IP addresses to be used
 #   as the fallback NTP servers. Any per-interface NTP servers obtained from
 #   systemd-networkd take precedence over this setting. requires puppetlabs-inifile
+#
+# @param ensure
+#   The state that the ``networkd`` service should be in
+#
+# @param package_name
+#   Name of the systemd-timesyncd system package, if applicable.
+#
+# @param package_ensure
+#   State of the systemd-timesyncd package
+#
 class systemd::timesyncd (
-  Enum['stopped','running'] $ensure                    = $systemd::timesyncd_ensure,
-  Optional[Variant[Array,String]] $ntp_server          = $systemd::ntp_server,
-  Optional[Variant[Array,String]] $fallback_ntp_server = $systemd::fallback_ntp_server,
+  String $service_name,
+  Stdlib::Unixpath $config_path,
+  Variant[Array,String] $ntp_server,
+  Variant[Array,String] $fallback_ntp_server,
+  Enum['stopped','running'] $ensure = 'running',
+  Optional[String] $package_name = undef,
+  String $package_ensure = 'installed',
 ) {
 
-  $_enable_timesyncd = $ensure ? {
+  $_enable_service = $ensure ? {
     'stopped' => false,
     'running' => true,
     default   => $ensure,
   }
 
-  service { 'systemd-timesyncd':
-    ensure => $ensure,
-    enable => $_enable_timesyncd,
+  $_service_defaults = {
+    'ensure' => $ensure,
+    'enable' => $_enable_service,
+  }
+
+  if $package_name {
+    package { $package_name:
+      ensure => $package_ensure,
+    }
+
+    $_service_require = {
+      'require' => "Package[${package_name}]",
+    }
+  } else {
+    $_service_require = {}
+  }
+
+  service { $service_name:
+    * => $_service_defaults + $_service_require,
   }
 
   if $ntp_server {
@@ -39,8 +72,8 @@ class systemd::timesyncd (
       value   => $_ntp_server,
       setting => 'NTP',
       section => 'Time',
-      path    => '/etc/systemd/timesyncd.conf',
-      notify  => Service['systemd-timesyncd'],
+      path    => $config_path,
+      notify  => Service[$service_name],
     }
   }
 
@@ -55,8 +88,8 @@ class systemd::timesyncd (
       value   => $_fallback_ntp_server,
       setting => 'FallbackNTP',
       section => 'Time',
-      path    => '/etc/systemd/timesyncd.conf',
-      notify  => Service['systemd-timesyncd'],
+      path    => $config_path,
+      notify  => Service[$service_name],
     }
   }
 }
