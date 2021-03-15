2b2360541d115a4f391d53885bbaf0d17e2491db
jonasdemoor <17252323+jonasdemoor@users.noreply.github.com>
2021-03-15 14:08:39 +0100

    Parameterize logind class

--- manifests/logind.pp
+++ manifests/logind.pp
@@ -1,19 +1,38 @@
-# @api private
-#
 # This class manages systemd's login manager configuration.
 #
 # https://www.freedesktop.org/software/systemd/man/logind.conf.html
-class systemd::logind {
-  service { 'systemd-logind':
-    ensure => running,
+#
+# @param service_name
+#   Name of the logind service
+# @param config_path
+#   Filesystem path to the logind configuration file
+# @param service_ensure
+#   State that the logind service should be in
+# @param settings
+#   Config Hash that is used to configure settings in logind.conf
+# @param loginctl_users
+#   Config Hash that is used to generate instances of our type
+#   `loginctl_user`.
+#
+class systemd::logind(
+  String $service_name,
+  Stdlib::Unixpath $config_path,
+  Enum['running','stopped'] $service_ensure = 'running',
+  Systemd::LogindSettings $settings = {},
+  Hash $loginctl_users = {},
+) {
+
+  service { $service_name:
+    ensure => $service_ensure,
   }
-  $systemd::logind_settings.each |$option, $value| {
+
+  $settings.each |$option, $value| {
     ini_setting {
       $option:
-        path    => '/etc/systemd/logind.conf',
+        path    => $config_path,
         section => 'Login',
         setting => $option,
-        notify  => Service['systemd-logind'],
+        notify  => Service[$service_name],
     }
     if $value =~ Hash {
       Ini_setting[$option] {
@@ -30,7 +49,7 @@ class systemd::logind {
     }
   }
 
-  $systemd::loginctl_users.each |$loginctl_name, $loginctl_settings| {
+  $loginctl_users.each |$loginctl_name, $loginctl_settings| {
     loginctl_user { $loginctl_name:
       * => $loginctl_settings,
     }
