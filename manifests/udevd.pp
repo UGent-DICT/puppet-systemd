94d52bfb8b5c7a47bdd07304360efa1a375a4f4f
jonasdemoor <17252323+jonasdemoor@users.noreply.github.com>
2021-03-15 14:49:48 +0100

    Parameterize udevd class

--- manifests/udevd.pp
+++ manifests/udevd.pp
@@ -1,30 +1,67 @@
 # This class manages systemd's udev config
 #
 # https://www.freedesktop.org/software/systemd/man/udev.conf.html
-class systemd::udevd {
+#
+# @param service_name
+#   Name of the udevd service to manage
+# @param config_path
+#   Filesystem path to the udevd configuration file
+# @param service_ensure
+#   State that the udevd service should be in
+# @param enable_service
+#   Whether the udevd service should be started on system boot
+# @param udev_log
+#   The value of /etc/udev/udev.conf udev_log
+# @param udev_children_max
+#   The value of /etc/udev/udev.conf children_max
+# @param udev_exec_delay
+#   The value of /etc/udev/udev.conf exec_delay
+# @param udev_event_timeout
+#   The value of /etc/udev/udev.conf event_timeout
+# @param udev_resolve_names
+#   The value of /etc/udev/udev.conf resolve_names
+# @param udev_timeout_signal
+#   The value of /etc/udev/udev.conf timeout_signal
+# @param rules
+#   Config Hash that is used to generate instances of our
+#   `udev::rule` define.
+#
+class systemd::udevd(
+  String $service_name,
+  Stdlib::Unixpath $config_path,
+  Enum['running', 'stopped'] $service_ensure = 'running',
+  Boolean $enable_service = true,
+  Optional[Variant[Integer,String]] $udev_log = undef,
+  Optional[Integer] $udev_children_max = undef,
+  Optional[Integer] $udev_exec_delay = undef,
+  Optional[Integer] $udev_event_timeout = undef,
+  Optional[Enum['early', 'late', 'never']] $udev_resolve_names = undef,
+  Optional[Variant[Integer,String]] $udev_timeout_signal = undef,
+  Hash $rules = {},
+) {
 
-  service { 'systemd-udevd':
-    ensure => running,
-    enable => true,
+  service { $service_name:
+    ensure => $service_ensure,
+    enable => $enable_service,
   }
 
-  file { '/etc/udev/udev.conf':
+  file { $config_path:
     ensure  => 'file',
     owner   => 'root',
     group   => 'root',
     mode    => '0444',
     content => epp("${module_name}/udev_conf.epp", {
-        'udev_log'            => $systemd::udev_log,
-        'udev_children_max'   => $systemd::udev_children_max,
-        'udev_exec_delay'     => $systemd::udev_exec_delay,
-        'udev_event_timeout'  => $systemd::udev_event_timeout,
-        'udev_resolve_names'  => $systemd::udev_resolve_names,
-        'udev_timeout_signal' => $systemd::udev_timeout_signal,
+        'udev_log'            => $udev_log,
+        'udev_children_max'   => $udev_children_max,
+        'udev_exec_delay'     => $udev_exec_delay,
+        'udev_event_timeout'  => $udev_event_timeout,
+        'udev_resolve_names'  => $udev_resolve_names,
+        'udev_timeout_signal' => $udev_timeout_signal,
     }),
-    notify  => Service['systemd-udevd'],
+    notify  => Service[$service_name],
   }
 
-  $systemd::udev_rules.each |$udev_rule_name, $udev_rule| {
+  $rules.each |$udev_rule_name, $udev_rule| {
     systemd::udev::rule { $udev_rule_name:
       * => $udev_rule,
     }
